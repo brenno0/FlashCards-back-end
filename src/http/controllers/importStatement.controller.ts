78519c4b24269afafbf627cdb67f4a726e1fe711
brennor <brennorassis@gmail.com>
2025-06-24 23:01:52 -0300

    Cleaning the code and improving the logic

--- src/http/controllers/importStatement.controller.ts
+++ src/http/controllers/importStatement.controller.ts
@@ -6,41 +6,42 @@ import { z } from 'zod';
 
 export const loadNubankBilling = async (request: FastifyRequest, reply: FastifyReply) => {
   try {
-    const parts = request.parts()
+    const parts = request.parts();
 
-    let file = null
-    let accountId: string | null = null
+    let file = null;
+    let accountId: string | null = null;
 
     for await (const part of parts) {
       if (part.type === 'file') {
-        file = part
+        file = part;
       } else if (part.type === 'field' && part.fieldname === 'accountId') {
-        accountId = part.value as string | null
+        accountId = part.value as string | null ;
       }
     }
 
-    if (!file) throw new NoCSVFileError()
-
     const transactionBodySchema = z.object({
       accountId: z.string().min(1),
-    })
+    });
 
-    const validatedFields = transactionBodySchema.parse({ accountId })
+    const { accountId: validatedAccountId } = transactionBodySchema.parse({ accountId });
 
-    const saveNubankTransactionsUseCase = makeNubankBilling()
-    const userId = request.user.sub
+    const saveNubankTransactionsUseCase = makeNubankBilling();
+    const userId = request.user.sub;
 
     const { records } = await saveNubankTransactionsUseCase.execute({
       data: file,
       userId,
-      accountId: validatedFields.accountId
-    })
+      accountId: validatedAccountId,
+    });
 
-    return reply.send({ message: 'CSV parsed successfully', data: records })
+    return reply.send({ message: 'CSV parsed successfully', data: records });
   } catch (error) {
-    if (error instanceof NoCSVFileError) return reply.status(400).send({ error: error.message })
-    if (error instanceof InvalidFileFormat) return reply.status(415).send({ error: error.message })
+    if (error instanceof NoCSVFileError)
+      return reply.status(400).send({ error: error.message });
+    if (error instanceof InvalidFileFormat)
+      return reply.status(415).send({ error: error.message });
 
-    return reply.status(500).send({ error: 'Internal server error' })
+    console.error(error);
+    return reply.status(500).send({ error: 'Internal server error' });
   }
-}
+};
--- src/use-cases/saveNubankTransactions.ts
+++ src/use-cases/saveNubankTransactions.ts
@@ -7,7 +7,7 @@ import { InvalidFileFormat } from './errors/invalidFileFormat';
 
 
 interface SaveNuBankTransactionsExecuteRequest {
-    data: MultipartFile | undefined;
+    data: MultipartFile | null;
     accountId: string;
     userId: string;
 }
@@ -16,31 +16,14 @@ export class SaveNubankTransactionsUseCase {
     constructor(private readonly transactionRepository:TransactionsRepository) {}
     
     async execute({ data, accountId, userId }: SaveNuBankTransactionsExecuteRequest) {
-        
-        if (!data ) throw new NoCSVFileError();
+
+        if (!data) throw new NoCSVFileError();
         if (data.mimetype !== 'text/csv' ) throw new InvalidFileFormat();
-
-        const chunks = []      
-
-        for await (const chunk of data.file) {
-            chunks.push(chunk)
-        }
         
-        const csvContent = Buffer.concat(chunks).toString('utf8');
-
-        const records:NubankCsvRequestDTO[] = await new Promise((resolve, reject) => {
-            
-        parse(csvContent, {
-            columns: true, 
-            skip_empty_lines: true,
-        }, (err, output) => {
-            if (err) reject(err);
-            resolve(output);
-        });
-        });
+        const records = await this.parseCsvAndReturnRecords(data)
 
         records.forEach(async record => {
-            const transactionType = record.amount.startsWith('-') ? 'EXPENSE' : 'INCOME'
+            const transactionType = record.amount.startsWith('-') ? 'INCOME' : 'EXPENSE'
             await this.transactionRepository.create({
                 amount:Number(record.amount),
                 date:new Date(record.date),
@@ -54,5 +37,29 @@ export class SaveNubankTransactionsUseCase {
         return { records }
         
 
+    }
+
+    async parseCsvAndReturnRecords(data:MultipartFile) {
+
+        // Read the CSV file
+        const chunks = []      
+        for await (const chunk of data.file) {
+            chunks.push(chunk)
+        }
+        
+        const csvContent = Buffer.concat(chunks).toString('utf8');
+        
+        // Transform CSV to JSON
+        const records:NubankCsvRequestDTO[] = await new Promise((resolve, reject) => {
+            parse(csvContent, {
+                columns: true, 
+                skip_empty_lines: true,
+            }, (err, output) => {
+                if (err) reject(err);
+                resolve(output);
+            });
+        });
+
+        return records;
     }
 }
